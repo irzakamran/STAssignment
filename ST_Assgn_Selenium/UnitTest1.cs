a27a7d688319f4081a4a5a4cd95c6fe7b6073fe1
Irza Kamran <irzakamran@yahoo.com>
2022-06-15 21:46:52 +0500

    search bar automated

---
+++ ST_Assgn_Selenium/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ST_Assgn_Selenium
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
